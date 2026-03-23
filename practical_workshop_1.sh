d544fa6e33368c850fff6fabc54e56592e22092b
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-03-23 12:32:20 +0000

    frist commit 23/03/2026

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -2,3 +2,7 @@ uname -a
 which gpg 
 gpg --version
 gpg --full-generate-key 
+gpg --list-keys
+gpg --armor --export israelfarfan992@gmail.com > mi_llave_publica.asc
+ls 
+cat mi_llave_publica.asc
---
+++ script_gnpg.sh
@@ -0,0 +1,2 @@
+gpg --list-secret-keys --keyid-format=long // This command lists the secret keys in GPG with their key IDs in long format.
+gpg --armor --export-secret-keys// This command exports the secret keys in ASCII armored
