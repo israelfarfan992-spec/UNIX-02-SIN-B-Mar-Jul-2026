488d5b8eb7b3b21f2320cf07f077719cf5cf8d47
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-03-23 12:59:01 +0000

    second commit 23/03/2026

--- script_gnpg.sh
+++ script_gnpg.sh
@@ -1,2 +1,5 @@
 gpg --list-secret-keys --keyid-format=long // This command lists the secret keys in GPG with their key IDs in long format.
 gpg --armor --export-secret-keys// This command exports the secret keys in ASCII armored
+gpg --armor --export-secret-keys > mi_llave_privada.asc // This command exports the secret keys in ASCII armored format and saves it to a file named "mi_llave_privada.asc".
+gpg --list-keys // This command lists the public keys in GPG.
+gpg --import llave_de_isaac.asc // This command imports a public key from a file named "llave_de_isaac.asc".
