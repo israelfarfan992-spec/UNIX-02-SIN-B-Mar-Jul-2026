9484c797fcf183bfa6977a40b9acb0222cdc2525
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-03-26 00:54:42 +0000

    FRIST BLOCK

---
+++ script_gnupg.sh
@@ -0,0 +1,5 @@
+"block A"
+which gpg // Check if gpg is installed
+gpg --version // Check the version of gpg
+gpg --list-keys // List all the keys in the keyring
+gpg --list-secret-keys --keyid-format=long // List all the secret keys with long key IDs
