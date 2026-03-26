1992542b155366d6209071b4a52da0a9d6199416
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-03-26 01:28:58 +0000

    block c

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -3,5 +3,8 @@ which gpg // Check if gpg is installed
 gpg --version // Check the version of gpg
 gpg --list-keys // List all the keys in the keyring
 gpg --list-secret-keys --keyid-format=long // List all the secret keys with long key IDs
- gpg --import mi_llave_publica_venegas.asc // Import a public key from a file
+gpg --import mi_llave_publica_venegas.asc // Import a public key from a file
+echo "este mensaje es secreto" >doc_no_cifrado.txt
+gpg --encrypt --recipient israelfarfan99@gmail.com doc_cifrado.tx
+gpg -- decrypt --recipient// Decrypt the file using the recipient's email 
 
