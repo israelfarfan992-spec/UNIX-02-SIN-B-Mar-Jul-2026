9569fba9156f62530d64d1d0b33b0490df605504
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-03-26 01:17:27 +0000

    second block

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -3,3 +3,5 @@ which gpg // Check if gpg is installed
 gpg --version // Check the version of gpg
 gpg --list-keys // List all the keys in the keyring
 gpg --list-secret-keys --keyid-format=long // List all the secret keys with long key IDs
+ gpg --import mi_llave_publica_venegas.asc // Import a public key from a file
+
