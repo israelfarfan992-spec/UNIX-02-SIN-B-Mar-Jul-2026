3c5b900e885e6cf09a6b99ecf46e750dcd5a2625
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-03-30 12:51:59 +0000

    frsit commit 30/03/2026

---
+++ scripts.sh
@@ -0,0 +1,4 @@
+sudo upt update // Update the package lists for upgrades and new packages
+sudo apt upgrade // Upgrade the installed packages to their latest versions
+sudo apt install parted // Install the parted package, which is a disk partitioning tool
+
