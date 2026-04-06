1f117571670a46440ad5c2174325fa65540753d5
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-06 12:51:54 +0000

    next commint

--- scrpit_boot.sh
+++ scrpit_boot.sh
@@ -8,3 +8,14 @@ ls -lai // # List all files and directories in the current directory with detail
 total 12
 925748 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:34 .
 918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:34 .. 
+stat .// # Display detailed information about the current directory (.)
+  File: .
+  Size: 4096            Blocks: 8          IO Block: 4096   directory
+Device: 0,45    Inode: 925748      Links: 2
+Access: (0755/drwxr-xr-x)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+Access: 2026-04-06 12:34:53.263949014 +0000
+Modify: 2026-04-06 12:34:35.175948103 +0000
+Change: 2026-04-06 12:34:35.175948103 +0000
+ Birth: 2026-04-06 12:34:35.175948103 +0000
+ #are diferrent ammount IO BLOCKS because each block is 4096 bytes and the directory itself takes up 4096 bytes, 
+ #but it may also have additional blocks allocated for storing metadata or other information.
