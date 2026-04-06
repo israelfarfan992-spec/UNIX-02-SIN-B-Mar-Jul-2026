44d0dee65a7abde96eab38c9e651928588329885
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-06 12:58:10 +0000

    next commint

--- scrpit_boot.sh
+++ scrpit_boot.sh
@@ -19,3 +19,15 @@ Change: 2026-04-06 12:34:35.175948103 +0000
  Birth: 2026-04-06 12:34:35.175948103 +0000
  #are diferrent ammount IO BLOCKS because each block is 4096 bytes and the directory itself takes up 4096 bytes, 
  #but it may also have additional blocks allocated for storing metadata or other information.
+mkdir -p /tmp/prueba/sub1/tmp/prueba/sub2 // # Create a nested directory structure with the -p option to create parent directories as needed
+stat /tmp/prueba // # Display detailed information about the /tmp/prueba directory
+  File: /tmp/prueba
+  Size: 4096            Blocks: 8          IO Block: 4096   directory
+Device: 8,17    Inode: 393231      Links: 3
+Access: (0756/drwxr-xrw-)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+Access: 2026-04-06 12:52:38.508002665 +0000
+Modify: 2026-04-06 12:52:38.508002665 +0000
+Change: 2026-04-06 12:52:38.508002665 +0000
+ Birth: 2026-04-06 12:52:38.508002665 +0000
+man mkdir // # Display the manual page for the mkdir command to learn more about its options and usage 
+ 
