aa177f29d80485c028d3eaa3fa5edc6f03bbd9fe
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-06 12:36:52 +0000

    comments and mkdir

--- scrpit_boot.sh
+++ scrpit_boot.sh
@@ -1,4 +1,7 @@
-cd //
-cd /home/codespace //
-cd ~ //
-cd $HOME //
+cd // # Change directory to the root filesystem
+cd /home/codespace // # Change directory to the /home/codespace directory
+cd ~ // # Change directory to the user's home directory (tilde expansion)
+cd $HOME // # Change directory to the user's home directory using the $HOME environment variable
+mkdir proyecto // # Create a new directory named "proyecto"
+cd proyecto // # Change directory to the newly created "proyecto" directory
+ls -lai // # List all files and directories in the current directory with detailed information
