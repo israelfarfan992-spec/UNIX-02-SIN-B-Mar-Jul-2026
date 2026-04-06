0216a23a26da1b9af92d4944dc189057adeba789
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-06 12:40:38 +0000

    ls -lai

--- scrpit_boot.sh
+++ scrpit_boot.sh
@@ -5,3 +5,6 @@ cd $HOME // # Change directory to the user's home directory using the $HOME envi
 mkdir proyecto // # Create a new directory named "proyecto"
 cd proyecto // # Change directory to the newly created "proyecto" directory
 ls -lai // # List all files and directories in the current directory with detailed information
+total 12
+925748 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:34 .
+918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:34 .. 
