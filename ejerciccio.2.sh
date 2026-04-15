dec639a12cb191bd624630e7ee3ea28a075f1a24
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-15 13:36:11 +0000

    second ejercice

---
+++ ejerciccio.2.sh
@@ -0,0 +1,5 @@
+touch prueba.txt # Create a new file named prueba.txt in the current directory
+chmod 600 prueba.txt # Set permissions to read/write for the owner and no permissions for group and others
+ls -l prueba.txt # Verify the permissions of the file
+chmod 755 prueba.txt # Change permissions to read/write/execute for owner and read/execute for group and others
+ls -l prueba.txt # Verify the new permissions of the file
