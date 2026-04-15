1581b6fdb0eb229399e78ab06969ef902a923c96
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-15 13:09:31 +0000

    chmod

--- ejercicio.1.sh
+++ ejercicio.1.sh
@@ -3,3 +3,4 @@ echo 'echo "Hola desde mi primer script"' >> hola.sh # Add a line to the script
 cat hola.sh # Display the contents of the script to verify it was created correctly
 ./hola.sh # Execute the script to see the output "Hola desde mi primer script"
 ls -l hola.sh # List the details of the hola.sh file
+chmod +x hola.sh # Make the script executable
