1b74bf7149751ea37e837479a8a5a5d8accffd0c
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-15 13:09:53 +0000

    hola

--- ejercicio.1.sh
+++ ejercicio.1.sh
@@ -4,3 +4,4 @@ cat hola.sh # Display the contents of the script to verify it was created correc
 ./hola.sh # Execute the script to see the output "Hola desde mi primer script"
 ls -l hola.sh # List the details of the hola.sh file
 chmod +x hola.sh # Make the script executable
+ls -l hola.sh # Verify that the execute permission has been added
