249661fe0c14687f85a4a53f502667f19a953a08
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-15 13:09:10 +0000

    ls

--- ejercicio.1.sh
+++ ejercicio.1.sh
@@ -2,3 +2,4 @@ echo '#!/bin/sh' > hola.sh # Create a new shell script named hola.sh with the sh
 echo 'echo "Hola desde mi primer script"' >> hola.sh # Add a line to the script
 cat hola.sh # Display the contents of the script to verify it was created correctly
 ./hola.sh # Execute the script to see the output "Hola desde mi primer script"
+ls -l hola.sh # List the details of the hola.sh file
