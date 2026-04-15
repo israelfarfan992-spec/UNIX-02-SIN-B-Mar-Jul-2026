b52376d0e42d000c6b4b30681ca388e874f4ea84
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-15 13:01:38 +0000

    cat

--- ejercicio.1.sh
+++ ejercicio.1.sh
@@ -1,2 +1,3 @@
 echo '#!/bin/sh' > hola.sh # Create a new shell script named hola.sh with the shebang line for sh
 echo 'echo "Hola desde mi primer script"' >> hola.sh # Add a line to the script
+cat hola.sh # Display the contents of the script to verify it was created correctly
