886dfa7df2aadb85f3bf01aa51add08a2b3ef24c
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-04-15 13:21:18 +0000

    sudos

--- ejercicio.1.sh
+++ ejercicio.1.sh
@@ -9,6 +9,6 @@ ls -l hola.sh # Verify that the execute permission has been added
 
 ls /etc # List the contents of the /etc directory, which contains system configuration files
 
-touch /etc/prueba.txt # Create a new file named prueba.txt in the /etc directory
+touch /etc/prueba.txt # Create a new file named prueba.txt in the /etc directory (this need sudo)
 mkdir ~/mi_carpeta # Create a new directory named mi_carpeta in the user's home directory
-apt install cowsay # Install the cowsay package, which is a fun command that generates ASCII art of a cow saying a message
+apt install cowsay # Install the cowsay package, which is a fun command that generates ASCII art of a cow saying a message (this need sudo)
