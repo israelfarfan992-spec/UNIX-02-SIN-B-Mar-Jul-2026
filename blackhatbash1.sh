39a2e810f0c219b1f9b58b0e23de30c2509d926a
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-01 13:11:10 +0000

    commtit

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,3 +1,25 @@
+#!/bin/bash -x
+#this option prints all coomands and their arguments
+#!/bin/bash -r 
+#this option prints all commands and their arguments after expansion in restricted mode
+#bash -n script.sh  this option checks the syntax of the script without executing it
+
 bash --version
-env
+env 
+
 echo ${SHELL}
+echo ${RANDOM}
+echo ${UID}
+echo ${OSTYPE}
+
+ps -ef
+df --human-readable #df -h
+
+chmod 777 blackhatbash1.sh
+./blackhatbash1.sh
+bash -r blackhatbash1.sh #This option creates a restricted bash shell, which restricts certain potentially dangerous commands.
+
+
+bash -n black
+
+
