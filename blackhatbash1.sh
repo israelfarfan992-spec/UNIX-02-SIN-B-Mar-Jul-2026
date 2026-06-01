4d34201237a24cf6b405ff996ff9b0a53be5f00b
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-01 13:17:53 +0000

    set -x set +x

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
-#this option prints all coomands and their arguments
-#!/bin/bash -r 
-#this option prints all commands and their arguments after expansion in restricted mode
-#bash -n script.sh  this option checks the syntax of the script without executing it
+set -x
 
-bash --version
+--snip--
+
+set +x
+
+#bash --version
 env 
 
 echo ${SHELL}
@@ -16,10 +17,10 @@ ps -ef
 df --human-readable #df -h
 
 chmod 777 blackhatbash1.sh
-./blackhatbash1.sh
-bash -r blackhatbash1.sh #This option creates a restricted bash shell, which restricts certain potentially dangerous commands.
+#./blackhatbash1.sh
+#bash -r blackhatbash1.sh #This option creates a restricted bash shell, which restricts certain potentially dangerous commands.
 
 
-bash -n black
+#bash -n black
 
 
