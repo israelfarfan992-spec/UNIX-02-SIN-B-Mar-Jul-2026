e292b51a31fe7a48d44fa9233017faab93c48b78
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-03 12:45:54 +0000

    unset

--- array.sh
+++ array.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-IP_ADRESSES=(192.168.1.1 192.168.1.2 192.168.1.3 )
+IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3 )
 
-echo "${IP_ADRESSES[*]}"
+echo "${IP_ADDRESSES[*]}"
 
-echo "${IP_ADRESSES[0]}"
+echo "${IP_ADDRESSES[0]}"
+
+unset IP_ADDRESSES[1]
+
+echo "${IP_ADDRESSES[*]}"
 
