099d5743a5145d6dba598983691444b551cfe74a
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-08 13:10:34 +0000

    adsad

---
+++ input _prompting.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "What is your frist name?"
+read -r firstname
+
+echo "what is your last name?"
+read -r lastname
+
+echo "your frist name is ${firstname} and your last nma is 
+${lastname}"
