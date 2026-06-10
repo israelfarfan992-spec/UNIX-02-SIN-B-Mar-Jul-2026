600f2948ac2f394ab995b1698a7e72e0dcf8e859
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-10 12:32:19 +0000

    varuabel one two

---
+++ string_comparison.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+VARIABLE_ONE="nostarch"
+VARIABLE_TWO="nostarch"
+
+if  [[  "{VARIWBLE:ONE}"  ==  "{$VARIABLE_TWO}"  ]];then
+    echo "they are equal!"
+else
+ echo "they are equal"
+fi
