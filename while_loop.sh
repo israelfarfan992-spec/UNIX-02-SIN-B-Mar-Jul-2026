c07601d95e876b5033177c4163309ea8793dcefb
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-10 13:49:45 +0000

    a

---
+++ while_loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash  
+SIGNAL_TO_STOP_FILE="stoploop" 
+while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do
+echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..."
+ echo "Checking again in 2 seconds..."
+ sleep 2
+ done
+ echo "File was found! exiting..."
