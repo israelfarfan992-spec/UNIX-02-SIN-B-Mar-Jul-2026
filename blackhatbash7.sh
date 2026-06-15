c0481b7522493b9c130445ca25bd864ad962bbb3
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-15 13:14:28 +0000

    commit

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -2,3 +2,6 @@
 grep "35.237.4.214" log.txt
 grep "35.237.4.214\|13.66.139.0" log.txt
 grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+grep -v "35.237.4.214" log.txt
+grep -o "35.237.4.214" log.txt
+awk '{print $1}' log.txt
