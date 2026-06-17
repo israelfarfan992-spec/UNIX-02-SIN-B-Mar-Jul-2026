084a326a976a64d01a6656bde56d3704a35826a6
israelfarfan992-spec <israelfarfan992@gmail.com>
2026-06-17 13:08:53 +0000

    .

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -1,6 +1,24 @@
 #! /bin/bash
+awk '{print $1,$2,$3}' log.txt
 awk '{print $2}' log.txt
-awk '{print $1}' log.txt
 awk '{print $3}' log.txt
+awk '{print $1}' log.txt
+
 awk '{print $1,$NF}' log.txt
 awk '{print $NF}' log.txt
+
+awk -F',' '{print $1}' example_csv.txt
+
+awk 'NR < 10' log.txt
+
+grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/Mozilla/Godzilla/g' log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep "Godzilla" newlog.txt 
+
+sed 's/ //g' log.txt
+sed '1d' log.txt
+sed '$d' log.txt
+sed -n '2,15 p' log.txt
+sed -i '1d' log.txt
